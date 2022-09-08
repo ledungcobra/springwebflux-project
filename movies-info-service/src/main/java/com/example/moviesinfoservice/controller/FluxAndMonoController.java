404c506ae8978ebb760f065ac30a1a18524705fb
ledungcobra <ledungcobra@gmail.com>
2022-09-08 10:13:47 +0700

    Init project

---
+++ movies-info-service/src/main/java/com/example/moviesinfoservice/controller/FluxAndMonoController.java
@@ -0,0 +1,21 @@
+package com.example.moviesinfoservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> flux() {
+        return Flux.just(1, 2, 3, 4)
+                .log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<Integer> mono() {
+        return Mono.just(2222).log();
+    }
+}
